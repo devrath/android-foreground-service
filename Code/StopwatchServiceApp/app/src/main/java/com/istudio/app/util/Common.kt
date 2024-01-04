25e6b9fb6999c9f0d39cbc9dfc86a1653f67531c
Devrath <devrath.dev595@gmail.com>
2024-01-04 13:10:02 +0530

    added

---
+++ Code/StopwatchServiceApp/app/src/main/java/com/istudio/app/util/Common.kt
@@ -0,0 +1,9 @@
+package com.istudio.app.util
+
+fun formatTime(seconds: String, minutes: String, hours: String): String {
+    return "$hours:$minutes:$seconds"
+}
+
+fun Int.pad(): String {
+    return this.toString().padStart(2, '0')
+}
